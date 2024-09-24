0677bb00f49ce6ebdfef857a0c5dfe4da31105d9
Zidanrmdn <zidan071021@gmail.com>
2024-09-24 21:16:48 +0700

    Add files via upload

---
+++ zidan.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 22 23:08:44 2024
+
+@author: ASUS
+"""
+
+print("hitung luas ruangan")
+panjang = float(input("panjang: "))
+lebar = float(input("lebar: "))
+satuan = input("cm/m: ")
+A = panjang * lebar
+print("hasilnya adalah:", A, satuan)
+
