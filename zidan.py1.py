916243b87ddccc952f6da946cb54e1a67e9ff0f3
Zidanrmdn <zidan071021@gmail.com>
2024-09-24 21:17:41 +0700

    Add files via upload

---
+++ zidan.py1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 24 20:56:18 2024
+
+@author: ASUS
+"""
+
+nama = input("nama kamu siapa?")
+asal = input("dari mana asal kamu?")
+asal_sekolah = input("sebelumnya bersekolah dimana?")
+print ("hallo selamat siang", nama)
+print ("wah dari",asal)
+print ("pasti menyenangkan di",asal_sekolah)z
